ce0b0daa43ed690960ef3274414b26ce351c7afd
ibrag1mov <ibragimov.abdulhay@mail.ru>
2022-12-14 14:17:22 +0500

    initial commit

--- js/main.js
+++ js/main.js
@@ -21,9 +21,9 @@ for(pokemon of pokemons){
             </p>
         </div>
         <p class="text-center">${pokemon.type}</p>
-       <div class="wrapper px-1 py-2 d-flex justify-content-between align-items-center">
+       <div class="wrapper px-4 py-2 d-flex justify-content-between align-items-center">
             <p class="time">${pokemon.spawn_time}</p>
-            <button type="button" class="btn btn-primary mx-2">More</button>
+            <button type="button" class="btn btn-primary ">More</button>
        </div>
     `
 
