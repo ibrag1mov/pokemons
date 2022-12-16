852449b3a0a66497fe53b63e1d4f72d7e444f517
ibrag1mov <ibragimov.abdulhay@mail.ru>
2022-12-16 14:37:08 +0500

    scdsc

--- js/main.js
+++ js/main.js
@@ -1,40 +1,47 @@
 var cardWrapper=document.querySelector('.card-wrapper');
+let elSelect=document.querySelector('.js-select');
 
-function pokemonFunc(pokemons){
-    for(pokemon of pokemons){
+for(pokemon of pokemons){
   
-        let card=document.createElement('div');
-        card.setAttribute('class', 'card pt-4 my-2 px-2 text-white m-1 col-3');
+    let card=document.createElement('div');
+    card.setAttribute('class', 'card pt-4 my-2 px-2 text-white m-1 col-3');
+
+    card.innerHTML=`
+        <strong class="pocemon-id px-4 fs-2">${pokemon.id}</strong>
+        <div class="text-center mt-4">
+            <img src="${pokemon.img}" alt="${pokemon.name}">
+        </div>
+        <h2 class="text-center my-2">${pokemon.name}</h2>
+        <div class="d-flex justify-content-evenly">
+            <p class="fs-5 text-center">weight 
+                <span class="d-block size"> ${pokemon.weight} </span>
+            </p>
+            <p class="fs-5">height
+                <span class="d-block size"> ${pokemon.height} </span>
+            </p>
+        </div>
+        <p class="text-center">${pokemon.type}</p>
+       <div class="wrapper px-4 py-2 d-flex justify-content-between align-items-center">
+            <p class="time">${pokemon.spawn_time}</p>
+            <button type="button" class="btn btn-primary ">More</button>
+       </div>
+    `;
+
+
+   
+
+    cardWrapper.appendChild(card);
     
-        card.innerHTML=`
-            <strong class="pocemon-id px-4 fs-2">${pokemon.id}</strong>
-            <div class="text-center mt-4">
-                <img src="${pokemon.img}" alt="${pokemon.name}">
-            </div>
-            <h2 class="text-center my-2">${pokemon.name}</h2>
-            <div class="d-flex justify-content-evenly">
-                <p class="fs-5 text-center">weight 
-                    <span class="d-block size"> ${pokemon.weight} </span>
-                </p>
-                <p class="fs-5">height
-                    <span class="d-block size"> ${pokemon.height} </span>
-                </p>
-            </div>
-            <p class="text-center">${pokemon.type}</p>
-           <div class="wrapper px-4 py-2 d-flex justify-content-between align-items-center">
-                <p class="time">${pokemon.spawn_time}</p>
-                <button type="button" class="btn btn-primary ">More</button>
-           </div>
-        `;
-    
-    
-       
-    
-        cardWrapper.appendChild(card);
-        
-    
+ 
+    let newArr=[];
+    for (i of pokemon.type){
+        console.log(i);
+        if(!newArr.includes(i)){
+            newArr.push(i)
+        }
     }
     
+    
+    console.log(newArr);
 }
-
 
